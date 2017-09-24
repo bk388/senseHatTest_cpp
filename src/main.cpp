40f4642ac6eaaba764f6a53af8902443f70da09e
Bence Kasza <bk388@cam.ac.uk>
2017-09-24 12:32:53 +0100

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,70 @@
 // Author      : Bence
 // Version     :
 // Copyright   : 
-// Description : Main function for the project piVR
+// Description : Main function for the project senseHatTest_cpp
 //============================================================================
 
+#define FB_DEV_NAME "fb"
+
 #include "RTIMULib.h"
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <poll.h>
+#include <dirent.h>
+#include <string.h>
+
+#include <linux/input.h>
+#include <linux/fb.h>
+
+
+struct fb_t {
+	uint16_t pixel[8][8];
+};
+
+struct fb_t *fb;
+
+static int open_fbdev(const char *dev_name)
+{
+	struct dirent **namelist;
+	int i, ndev;
+	int fd = -1;
+	struct fb_fix_screeninfo fix_info;
+
+	ndev = scandir(DEV_FB, &namelist, is_framebuffer_device, versionsort);
+	if (ndev <= 0)
+		return ndev;
+
+	for (i = 0; i < ndev; i++)
+	{
+		char fname[64];
+		char name[256];
+
+		snprintf(fname, sizeof(fname),
+			 "%s/%s", DEV_FB, namelist[i]->d_name);
+		fd = open(fname, O_RDWR);
+		if (fd < 0)
+			continue;
+		ioctl(fd, FBIOGET_FSCREENINFO, &fix_info);
+		if (strcmp(dev_name, fix_info.id) == 0)
+			break;
+		close(fd);
+		fd = -1;
+	}
+	for (i = 0; i < ndev; i++)
+		free(namelist[i]);
+
+	return fd;
+}
+
 
 int main(int argc, char* argv[]) {
-    RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
-
+    /*RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
     RTIMU *imu = RTIMU::createIMU(settings);
     RTPressure *pressure = RTPressure::createPressure(settings);
     RTHumidity *humidity = RTHumidity::createHumidity(settings);
@@ -37,6 +90,22 @@ int main(int argc, char* argv[]) {
             //printf("%s\n", RTMath::displayDegrees("", imuData.fusionPose));
 	    printf("roll: %f\n", imuData.fusionPose.x());
         }
-    }
+    }*/
+
+	int fbfd = 0;
+	fbfd = open_fbdev("RPi-Sense FB");
+	if (fbfd <= 0) {
+		ret = fbfd;
+		printf("Error: cannot open framebuffer device.\n");
+		return -1;
+
+	}
+	fb = mmap(0, 128, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+	if (!fb) {
+		printf("Failed to mmap.\n");
+		return -1;
+	}
+	memset(fb, 0, 128);
+	fb->pixel[0][0] = 0xF800;
 }
 
