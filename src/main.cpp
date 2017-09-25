47772033e29c104c95860631f3fb43325d9403a2
Bence Kasza <bk388@cam.ac.uk>
2017-09-25 19:54:47 +0100

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,26 @@
 
 int main(int argc, char* args[]) {
 	LEDMatrix ledMat;
-	uint8_t image[8][8][3];
-	for(int ii=0;ii<8;ii++) {
-		image[ii][0][0] = 0xff;
-		image[ii][0][1] = 0xff;
-		image[ii][0][2] = 0xff;
+	while(1) {
+		uint8_t image[8][8][3];
+		for(int ii=0;ii<8;ii++) {
+			image[ii][0][0] = 0xff;
+			image[ii][0][1] = 0xff;
+			image[ii][0][2] = 0xff;
 
-		image[ii][7][0] = 0xff;
-		image[ii][7][1] = 0xff;
-		image[ii][7][2] = 0xff;
+			image[ii][7][0] = 0xff;
+			image[ii][7][1] = 0xff;
+			image[ii][7][2] = 0xff;
 
-		image[0][ii][0] = 0xff;
-		image[0][ii][1] = 0xff;
-		image[0][ii][2] = 0xff;
+			image[0][ii][0] = 0xff;
+			image[0][ii][1] = 0xff;
+			image[0][ii][2] = 0xff;
 
-		image[7][ii][0] = 0xff;
-		image[7][ii][1] = 0xff;
-		image[7][ii][2] = 0xff;
+			image[7][ii][0] = 0xff;
+			image[7][ii][1] = 0xff;
+			image[7][ii][2] = 0xff;
+		}
+		ledMat.showImage(image);
+		delete image;
 	}
-	ledMat.showImage(image);
-	while(1);
 }
