16b56887ceb33b6d084afb41b9aea396b0cb309f
Bence Kasza <bk388@cam.ac.uk>
2017-09-25 19:44:28 +0100

    ...

--- src/ledMatrix.cpp
+++ src/ledMatrix.cpp
@@ -1,19 +1,3 @@
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif //_GNU_SOURCE
-#define DEV_FB "/dev"
-#define FB_DEV_NAME "fb"
-
-#include "RTIMULib.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <linux/fb.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include <dirent.h>
-#include <string.h>
 #include "ledMatrix.h"
 
 /* Constructor of LEDMatrix */
