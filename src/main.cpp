73226142fb07f8c17419802830f4019e5a2b174e
Bence Kasza <bk388@cam.ac.uk>
2017-09-30 17:26:32 +0100

    started working on bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@
 
 #include "RTIMULib.h"
 #include "ledMatrix.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
 #include <math.h>
 
 #define EULER_NUM (float)2.71828
@@ -24,7 +31,7 @@ int main(int argc, char* args[]) {
 	uint8_t colour[3] = {255, 255, 255};
 	origin[0] = stringToNum(args[1]);
 	origin[1] = stringToNum(args[2]);
-	while(1) {
+	/*while(1) {
 		uint8_t image[8][8][3];
 		for(int ii=0;ii<8;ii++) {
 			image[ii][0][0] = 0xff;
@@ -44,7 +51,42 @@ int main(int argc, char* args[]) {
 			image[7][ii][2] = 0xff;
 		}
 		drawLine(origin, stringToNum(args[3])*CONST_PI/180.0, 5, colour, stringToNum(args[4]));
+	}*/
+	inquiry_info *ii = NULL;
+	int max_rsp, num_rsp;
+	int dev_id, sock, len, flags;
+	int i;
+	char addr[19] = { 0 };
+	char name[248] = { 0 };
+
+	dev_id = hci_get_route(NULL);
+	sock = hci_open_dev( dev_id );
+	if (dev_id < 0 || sock < 0) {
+		perror("opening socket");
+		exit(1);
 	}
+
+	len  = 8;
+	max_rsp = 255;
+	flags = IREQ_CACHE_FLUSH;
+	ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+
+	num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+	if( num_rsp < 0 ) perror("hci_inquiry");
+
+	for (i = 0; i < num_rsp; i++) {
+		ba2str(&(ii+i)->bdaddr, addr);
+		memset(name, 0, sizeof(name));
+		if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
+			name, 0) < 0)
+		strcpy(name, "[unknown]");
+		printf("%s  %s\n", addr, name);
+	}
+
+	free( ii );
+	close( sock );
+	return 0;
+
 }
 
 static void drawLine(float origin[2], double angle, float length, uint8_t colour[3], double sharpness) {
